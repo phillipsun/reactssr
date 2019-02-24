39cdb326123268d8fa3c15953d501718b4da01eb
Phillip Sun <psun.04@gmail.com>
2019-02-23 22:49:47 -0600

    got ssr working

--- build/bundle.js
+++ build/bundle.js
@@ -7026,14 +7026,26 @@ if (process.env.NODE_ENV === 'production') {
 "use strict";
 
 
-var express = __webpack_require__(53);
-var React = __webpack_require__(15);
-var renderToString = __webpack_require__(111).renderToString;
-var Home = __webpack_require__(122).default;
-var app = express();
+var _express = __webpack_require__(53);
+
+var _express2 = _interopRequireDefault(_express);
+
+var _react = __webpack_require__(15);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(111);
+
+var _Home = __webpack_require__(122);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
 
 app.get("/", function (req, res) {
-  var content = renderToString(React.createElement(Home, null));
+  var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
   res.send(content);
 });
@@ -22757,7 +22769,18 @@ var Home = function Home() {
   return _react2.default.createElement(
     "div",
     null,
-    "Home Component"
+    _react2.default.createElement(
+      "div",
+      null,
+      "VERY Best Home Component"
+    ),
+    _react2.default.createElement(
+      "button",
+      { onClick: function onClick() {
+          return console.log("Hello there");
+        } },
+      "Press me!"
+    )
   );
 };
 
--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home Component</div>;
+  return (
+    <div>
+      <div>VERY Best Home Component</div>
+      <button onClick={() => console.log("Hello there")}>Press me!</button>
+    </div>
+  );
 };
 
 export default Home;
